01e25f050cd153c7841f93a80df518fc38894840
Steve Nguyen <steve.nguyen@pollen-robotics.com>
2021-11-24 11:40:56 +0100

    gazebo working

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -23,45 +23,53 @@ def generate_launch_description():
             name='gui',
             default_value='True',
             description='Use joint_state_publisher_gui.'
-            ),
+        ),
         launch.actions.DeclareLaunchArgument(
             name='head',
             default_value='True',
             description='Add the head in Reachy configuration.',
-            ),
+        ),
         launch.actions.DeclareLaunchArgument(
             name='right_arm',
             default_value='True',
             description='Add the right arm in Reachy configuration.',
-            ),
+        ),
         launch.actions.DeclareLaunchArgument(
             name='left_arm',
             default_value='True',
             description='Add the left arm in Reachy configuration.',
-            ),
+        ),
         Node(
             package='joint_state_publisher',
             executable='joint_state_publisher',
-            condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui')),
+            condition=launch.conditions.UnlessCondition(
+                LaunchConfiguration('gui')),
         ),
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
-            condition=launch.conditions.IfCondition(LaunchConfiguration('gui')),
+            condition=launch.conditions.IfCondition(
+                LaunchConfiguration('gui')),
         ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
+            # parameters=[{
+            #     'robot_description': Command([
+            #         'xacro ',
+            #         xacro_path,
+            #         ' head:=',
+            #         LaunchConfiguration('head', default='true'),
+            #         ' right_arm:=',
+            #         LaunchConfiguration('right_arm', default='true'),
+            #         ' left_arm:=',
+            #         LaunchConfiguration('left_arm', default='true'),
+            #     ])}],
+
             parameters=[{
                 'robot_description': Command([
                     'xacro ',
-                    xacro_path,
-                    ' head:=',
-                    LaunchConfiguration('head'),
-                    ' right_arm:=',
-                    LaunchConfiguration('right_arm'),
-                    ' left_arm:=',
-                    LaunchConfiguration('left_arm'),
+                    xacro_path
                 ])}],
         ),
         Node(
